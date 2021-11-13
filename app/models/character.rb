b83969a5af41fdc786d540ecfd1a9f7df62f71c2
Gonzalo Oyanedel Vial <goyanedelv@gmail.com>
2021-11-13 19:24:29 +0000

    done

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,13 +10,40 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  def movie
-    key = self.movie_id
+  # def movie
+  #  key = self.movie_id
 
-    matching_set = Movie.where({ :id => key })
+  #  matching_set = Movie.where({ :id => key })
 
-    the_one = matching_set.at(0)
+  #  the_one = matching_set.at(0)
 
-    return the_one
-  end
+  #  return the_one
+  # end
+
+  # belongs_to(:movie,
+  #    {
+  #      :foreign_key => "movie_id",
+  #      :class_name => "Movie"
+  #    }
+  #  )
+  
+  # Even more abbreviated
+  # belongs_to(:movie,
+  #    {
+  #      :foreign_key => "movie_id",
+  #    }
+  #  )
+
+  # Even even even! more abbreviated
+  belongs_to(:movie)
+
+  belongs_to(:actor,
+    {
+      :foreign_key => "actor_id",
+      :class_name => "Actor"
+    }
+  )
+  
+
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,13 +13,53 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
-  def director
-    key = self.director_id
 
-    matching_set = Director.where({ :id => key })
+  # Only used for the the_one relation
+  belongs_to(:director,
+      {
+        :foreign_key => "director_id",
+        :class_name => "Director"
+      }
+    )
 
-    the_one = matching_set.at(0)
+  # def director
+  #  key = self.director_id
 
-    return the_one
-  end
+  #  matching_set = Director.where({ :id => key })
+
+  #  the_one = matching_set.at(0)
+
+  #  return the_one
+  # end
+
+  # And for the many relation:
+  has_many(:characters, 
+      {
+        :class_name => "Character", 
+        :foreign_key => "movie_id"
+      }
+    )
+  
+  # def characters
+  #  key =self.id
+
+  #  the_many = Characters.where({}:movie_id => key)
+
+  #  return the_many
+  # end
+
+  has_many(:cast, {:through => :characters, :source => :actor})
+
+  # def cast
+  #  the_many = Array.new
+
+  #  self.characters.each do |joining_record|
+  #    destination_record = joining_record.movie
+
+  #    the_many.push(destination_record)
+  #  end
+
+  #  return the_many
+  # end
+
 end
